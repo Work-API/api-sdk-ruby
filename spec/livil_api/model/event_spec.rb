3656578a272f9ca3b7fab3e6e0e46319dd2974d1
Dan de Havilland <dan@dandehavilland.com>
2020-02-12 09:39:20 +0100

    Fixing even model spec

--- spec/livil_api/model/event_spec.rb
+++ spec/livil_api/model/event_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe(LivilApi::Event) do
 
   context '#to_hash' do
     subject { event.to_hash }
-    it { is_expected.to include(attributes: Hash, id: String) }
+    it do
+      is_expected
+        .to include(
+          :all_day, :attachments, :attendees, :created_at, :description,
+          :end_date_time, :end_timezone, :integration_id, :location,
+          :name, :recurrence, :reminders, :remote_id, :start_date_time,
+          :start_timezone, :updated_at
+        )
+    end
   end
 end
